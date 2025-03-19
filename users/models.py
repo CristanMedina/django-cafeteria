657b1d19b313a7e79d18abf020f4b10528740d98
CristanMedina <crishighlights@gmail.com>
2025-03-19 11:57:41 -0700

    users model actualizado

--- users/models.py
+++ users/models.py
@@ -1,4 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        COCINERO = 'COCINERO', 'Cocinero'
+        CLIENTE = 'CLIENTE', 'Cliente'
 
+    base_role = Role.ADMIN
+
+    role = models.CharField(max_length=50, choices=Role.choices)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='users_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='users_permissions_set',
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+        super().save(*args, **kwargs)
+
+class ClienteManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.CLIENTE)
+
+class Cliente(User):
+    base_role = User.Role.CLIENTE
+
+    cliente = ClienteManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Solo para Clientes"
+
+class CocineroManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.COCINERO)
+
+class Cocinero(User):
+    base_role = User.Role.COCINERO
+
+    cocinero = CocineroManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Solo para Cocineros"
